7aa91bc69413e9b54521f872e18323121d4841b0
Perla Mendoza <pmdz70@gmail.com>
2016-03-13 01:30:53 -0800

    Borre carruzel y puse foto

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -4,51 +4,9 @@
 <!-- Slideshow Carrusel -->
 <div class="naranja">
     <div class="container">
-        <div class="carousel slide" id="myCarousel" data-ride="carousel">
-            <!-- Indicators -->
-            <ol class="carousel-indicators">
-                <li data class="active" data-slide-to="0" data-target="#myCarousel"></li>
-                <li data-slide-to="1"  data-target="#myCarousel"></li>
-                <li data-slide-to="2" data-target="#myCarousel"></li>
-                <li data-slide-to="3" data-target="#myCarousel"></li>
-            </ol>
-            <!-- Wrapper for slides -->
-            <div class="carousel-inner"> <!-- role="listbox-->
-
-                <div class="item active">
-                    <iframe src="https://player.vimeo.com/video/155624043" style="width:100%; height:100%" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
-                </div> <!-- end item-->
-
-                <div class="item">
-                    <img src="/images/SMLC2.png" class="img-responsive"  alt="Carrusel 2" style="width:100%; height:100%"></img>
-                    <img class="visible-xs" src="/images/SMLC2m.png"></img>
-                    <div class="carousel-caption">
-                        <div class="donar">
-                            <h4>Con Tu Apoyo </br>Creáremos </br> <span class="negro">Líderes</span></h4>
-                            <a href="https://nscef.ejoinme.org/MyPages/ElSymposiodeLatinas/tabid/732850/Default.aspx" class="button1">Donación</a>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="item">
-                    <img src="/images/SMLC3.png" class="img-responsive" alt="Carrusel3" style="width:100%; height:100%"/>
-                </div>
-
-                <div class="item">
-                    <img src="/images/SMLC4.png" class="img-responsive" alt="Carrusel4" style="width:100%; height:100%"/>
-                </div>
-            </div>
-
-            <!-- Left and right controls -->
-            <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-                <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
-            </a>
-            <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-                <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-                <span class="sr-only">Next</span>
-            </a>
-        </div>
+      <div class="cover">
+         <img class="img-responsive" src="/images/LatinasPrueba_1170x430.png"  style="width:100% height:320px";/>
+      </div>  
     </div> <!--Contanier-->
 </div> <!-- end naranaja -->
 

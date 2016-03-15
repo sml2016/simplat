76dc25d20b0841afcfac6271e8d195d1bb63b5a8
Perla Mendoza <pmdz70@gmail.com>
2016-03-15 16:42:23 -0700

    cambios en el footer y header

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,23 +1,57 @@
 <div class="footer">
     <div class="container clear">
-        <ul class="footerLinks clear">
-            <li><a href="{{ url('/historia') }}">Historia</a></li>
-            <li><a href="{{ url('/testimonios') }}">Testimonios</a></li>
-            <li><a href="{{ url('/grupos') }}">Grupos</a></li>
-            <li><a href="{{ url('/galeria/2014') }}">Galería 2014</a></li>
-            <li><a href="{{ url('/galeria/2015') }}">Galería 2015</a></li>
-            <li><a href="{{ url('/conferencias/2014') }}">Conferencias 2014</a></li>
-            <li><a href="{{ url('/conferencias/2015') }}">Conferencias 2015</a></li>
-            <li><a href="{{ url('/contacto') }}">Contacto</a></li>
-        </ul>
+     
+        
+          <ul class="footerLinks clear">
+            <div class="row">
+             <div class="col-sm-1 col-sm-offset-1">
+              <li><a href="{{ url('/historia') }}">Historia</a></li>
+             </div> <!--Col-->
+            
+               <div class="col-sm-2">
+                <div class="footerLinks clear">   
+                 <li><a href="{{ url('/testimonios') }}">Testimonios</a></li>
+                </div>
+               </div>  <!--Col-->
+               
+               <div class="col-sm-1">
+                <div class="footerLinks clear">     
+                 <li><a href="{{ url('/grupos') }}">Grupos</a></li>
+                 </div>
+                  </div> <!--Col-->
+            
+            <div class="col-sm-2">
+              <div class="footerLinks clear"> 
+               <li><a href="{{ url('/galeria/2014') }}">Galería 2014</a></li>
+                <li><a href="{{ url('/galeria/2015') }}">Galería 2015</a></li>
+               </div>
+             </div> <!--Col-->
+             
+              <div class="col-sm-2">
+               <div class="footerLinks clear"> 
+               <li><a href="{{ url('/conferencias/2014') }}">Conferencias 2014</a></li>
+               <li><a href="{{ url('/conferencias/2015') }}">Conferencias 2015</a></li>
+              </div>
+            </div>  <!--Col-->
+            
+              <div class="col-sm-1">
+              <div class="footerLinks clear"> 
+               <li><a href="{{ url('/contacto') }}">Contacto</a></li>
+              </div>
+              </div>   <!--Col-->
+            </div> <!--Endo row -->
+           </ul>
+        
         <div class="socialmedia">
-            <span>Síguenos Hoy En</span>
+            <h1>SML</h1>
             <ul>
                 <li><a href="https://www.facebook.com/latinasymposium/"><i class="fa fa-2x fa-facebook"></i></a></li>
                 <li><a href="https://twitter.com/SimposioLatinas"><i class="fa fa-2x fa-twitter"></i></a></li>
                 <li><a href="https://www.instagram.com/simposiomujereslatinas/"><i class="fa fa-2x fa-instagram"></i></a></li>
             </ul>
-        </div>
+           </div>
+         </div>
+         
         <hr class="clear">
         <p> Diseñado por Perla Mendoza & Luis García Alanís <br> © 2016 Simposio de Mujeres Latinas </p>
     </div>

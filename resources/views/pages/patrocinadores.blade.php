e81c2d706ee41c7fbeb5d43e35867017f54e477c
Perla Mendoza <pmdz70@gmail.com>
2016-03-18 16:58:12 -0700

    agregue la pag de patrocinadores

--- resources/views/pages/patrocinadores.blade.php
+++ resources/views/pages/patrocinadores.blade.php
@@ -1,4 +1,69 @@
 @extends('layouts.master')
 @section('content')
-<h1>Patrocinadores!</h1>
+<div class="naranjaR"></div> <!-- naranjaR -->
+ <div class="container">
+   <div class="fondo1"> 
+        
+    <div class="patrocinadores">
+     <h1>¡Gracias a su apoyo este evento es posible!</h1>
+        
+      <!---Patrocinadores Primeros--->    
+      <div class="row">
+       <div class="col-sm-4">
+         <div class="thumbnail">
+            <img src="/images/patrocinadores/NSC.png" alt="NSC"/>
+             <p> ¡Gracias! </p>  
+            </div>
+           </div>
+
+            <div class="col-sm-4">
+             <div class="thumbnail">
+               <img src="/images/patrocinadores/latinofoundation.png" alt="LCF"/>
+                <p> ¡Gracias! </p>
+              </div>
+             </div>
+                
+             <div class="col-sm-4">
+              <div class="thumbnail">
+               <img src="/images/patrocinadores/NSC.png" alt="NSC"/>
+                <p> ¡Gracias! </p>
+              </div>
+             </div>
+          </div>  <!---End ROw -->
+                
+            <!---Patrocinadores 2nd--->    
+                <div class="row">
+                <div class="col-sm-3">
+                <div class="thumbnail">
+                <img src="/images/patrocinadores/latinofoundation.png" alt="NSC"/>
+                <p> ¡Gracias! </p>
+                </div>
+                </div>
+                
+               <div class="col-sm-3">
+                <div class="thumbnail">
+                <img src="/images/patrocinadores/NSC.png" alt="NSC"/>
+                <p> ¡Gracias! </p>
+                </div>
+                </div>
+                
+                 <div class="col-sm-3">
+                <div class="thumbnail">
+                <img src="/images/patrocinadores/NSC.png" alt="NSC"/>
+                <p> ¡Gracias! </p>
+                </div>
+                </div>
+                
+                <div class="col-sm-3">
+                <div class="thumbnail">
+                <img src="/images/patrocinadores/NSC.png" alt="NSC"/>
+                <p> ¡Gracias! </p>
+                </div>
+             </div>
+          </div>  <!---End ROw -->
+       </div> <!--End Patrocinadores -->
+
+    </div> <!-- Fondo1 -->
+ </div> <!-- container -->
 @stop
+

9ec45e183c7d4326d8347c5e324b99e5df73f22e
Perla Mendoza <pmdz70@gmail.com>
2016-03-23 00:54:06 -0700

    responsive en contacto enviado

--- resources/views/pages/contacto/create.blade.php
+++ resources/views/pages/contacto/create.blade.php
@@ -16,7 +16,7 @@
                 {!! csrf_field() !!}
                 <div class="form-group-lg">
                     <div class="row">
-                        <div class="col-xs-8 col-xs-offset-2 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                        <div class="col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                             @if ($errors->any())
                               @foreach ($errors->all() as $error)
                                   <div class="alert alert-danger" role="alert">{{ $error }}</div>

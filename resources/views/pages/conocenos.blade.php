bdbee2eb1a60e5065bf64e371c1579ff49829315
Perla Mendoza <pmdz70@gmail.com>
2016-03-24 07:03:56 -0700

    sigo con traducciones en conocenos

--- resources/views/pages/conocenos.blade.php
+++ resources/views/pages/conocenos.blade.php
@@ -21,14 +21,7 @@
              <h3>Comité de Registro y Canastas</h3>
              {!! trans('conocenos.Jessica') !!}
             <button type="button" class="btn btn-leer btn-sm">Leer más</button>
-                  
-            <p> Queridas Mujeres Latinas,
-             Mi nombre es Jessica Cabrera, tengo 51 años, soy de nacionalidad peruana, feliz madre de dos maravillosas hijas de 33 y 21 años y psicóloga de profesión. Emigre a los Estados Unidos hace poco más de 11 años. Convertida en madre soltera y con una niña pequeña de 10 diez años, me aleje de mi querida patria, familia, hogar, idioma, tradiciones y costumbres y llegue a este país cargada de muchos sueños, ilusiones y en busca de nuevas oportunidades y desafíos. </p>
-                   
-            <p> Unos de los desafíos más grandes a los cuales me tuve que enfrentar fue sin duda alguna la barrera del idioma, aunado al comienzo de una nueva vida en un país desconocido y la crianza de una niña pequeña. Afortunadamente al cabo de unos años tuve la oportunidad de estudiar el idioma y con mucha constancia y perseverancia aún sigo enfrentándome a nuevos retos los cuales me inspiran a seguir trabajando por lograr mis objetivos personales y profesionales. Dentro de la parte personal encontré al compañero de mi vida, que me apoya constantemente y cree en mis ideales.</p>
-            <p>Mientras mantenía un trabajo a tiempo completo y continuaba mis estudios en el college, siempre considere importante conocer de cerca cómo funciona el sistema educativo para que mi hija pudiera adaptarse rápidamente a la escuela y fue así como me fui involucrando poco a poco como voluntaria y fui participando como facilitadora y sirviendo como guía para muchos grupos de padres latinos, lo cual colaboró a la formación de lo que hoy en día se conoce como “Natural Leader” y llegue a convertirme en una de las primeras líderes naturales que tuvo el distrito escolar de Edmonds. </p>
-            <p>Actualmente pertenezco al comité de EAACH que significa Alianza Equitativa para el éxito, tuve la oportunidad de completar el programa de certificación de Mentores para Padres (Parent Mentor) en el Edmonds Community College, obtuve una certificación como Educadora de Padres en Disciplina positiva, y trabajo con el grupo F.L.Y. (Foreign Language for Youth) como profesora de español para varias Escuelas Primarias de diferentes distritos. 
-            Esta vida es un constante aprendizaje y todas y cada una de nosotras podemos lograr nuestros objetivos y sueños siempre y cuando nos apoyemos y luchemos juntas como mujeres fuertes que somos, y esperamos que nuestras historias sirvan a la vez de inspiración para muchas mujeres que buscan un medio de apoyo para el beneficio de su desarrollo personal, como mujer, como esposa y como profesional!. Las esperamos con muchas ansias!</p>
+              {!! trans('conocenos.Jessica1') !!}      
            </div> <!--End Caption--->
          </div> <!--End thumbnails-->
         </div> <!--End Col--->
@@ -58,16 +51,9 @@
                  <div class="caption">
                 <h2>Velia Lara</h2>
                <h3>Comité de Comida-Decoración</h3>
-               <p>Mi interés de participar en el Comité Organizador para el 3er Simposio de la Mujer Latina es: que por medio de esta oportunidad pueda apoyar y facilitar los recursos necesarios para empoderar a otras mujeres. Ya que yo creo firmemente en el papel tan importante que juega la mujer en la familia y en la sociedad.</p>
+                 {!! trans('conocenos.Velia') !!}
               <button type="button" class="btn btn-leer btn-sm">Leer más</button>
-                   
-                   <p>Y nosotras como mujeres inmigrantes en este país que pasamos por diferentes cambios y etapas necesitamos de ese apoyo que nos ayude a salir adelante. 
-                   En mi experiencia al llegar a vivir a este país fué una gran oportunidad de cambio, que también trajo consigo muchas situaciones en las cuales he buscado reinventarme día a día para poder lograr mis objetivos y no desfallecer en el intento y luchar por lo quiero.
-                   Ya que yo soy de las mujeres que creen que no hay motivos para abandonar nuestros sueños y que es posible poder alcanzar lo que queremos y llevar también al mismo tiempo una vida equilibrada con los diferentes roles que nos tocan vivir.</p>
-
-                  <p>Soy originaria de Puebla, México. Estoy casada y soy madre de dos hermosos niños. Tengo casi 11 años viviendo aquí en el estado de WA. Estudie Licenciatura en Turismo en mi país natal. Siempre he tenido el deseo de superarme continuamente y de aprender cosas nuevas cada día; ya que el saber te da poder. Por lo cual desde que llegue a este país ha sido un continuo aprendizaje y preparación ya que quiero ser un modelo positivo y ejemplo vivo para mis hijos y enseñarles que si se puede a pesar de las barreras y obstáculos que te presente la vida.</p> 
-
-                  <p>Actualmente trabajo en el Centro Familiar de Shoreline y de Northshore en la ciudad de Bothell como facilitadora del programa Aprender Jugando, el cual me ha dado la oportunidad de trabajar con familias de diferentes culturas. También obtuve una certificación como Educador de Padres en Disciplina Positiva y es algo que me encanta hacer compartir con otros padres nuevas técnicas para la crianza de nuestros hijos, que son nuestro gran futuro.</p>
+                  {!! trans('conocenos.Velia1') !!} 
                </div> <!--End Caption--->
                </div> <!--End thumbnails-->
                </div> <!--End Col--->

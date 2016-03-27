b0b18b97b4cc61c824dcc2a40be794a61e8f2c66
Perla Mendoza <pmdz70@gmail.com>
2016-03-27 10:48:05 -0700

    agregue logo de latinoadvocacy y responsive de la pag. patrocinadores y traduccion

--- resources/views/pages/patrocinadores.blade.php
+++ resources/views/pages/patrocinadores.blade.php
@@ -7,25 +7,33 @@
    <div class="fondo1"> 
         
     <div class="patrocinadores">
-     <h1>¡Gracias a su apoyo este evento es posible!</h1>
+     <h1>{{ trans('patrocinadores.Gracias') }}</h1>
         
       <!---Patrocinadores Primeros--->    
       <div class="row">
-       <div class="col-sm-4">
+       <div class="col-sm-3">
          <div class="thumbnail">
             <img src="/images/patrocinadores/NSC.png" alt="NSC"/>
              <h1> ¡Gracias! </h1>  
             </div>
            </div>
 
-            <div class="col-sm-4">
+            <div class="col-sm-3">
              <div class="thumbnail">
                <img src="/images/patrocinadores/latinofoundation.png" alt="LCF"/>
                 <h1> ¡Gracias! </h1>
               </div>
              </div>
-                
-             <div class="col-sm-4">
+            
+            
+            <div class="col-sm-3">
+             <div class="thumbnail">
+               <img src="/images/patrocinadores/latinofoundation.png" alt="LCF"/>
+                <h1> ¡Gracias! </h1>
+              </div>
+             </div>
+            
+             <div class="col-sm-3">
               <div class="thumbnail">
                <img src="/images/patrocinadores/NSC.png" alt="NSC"/>
                 <h1> ¡Gracias! </h1>
@@ -35,28 +43,88 @@
                 
             <!---Patrocinadores 2nd--->    
                 <div class="row">
-                <div class="col-sm-3">
+                <div class="col-sm-2">
                 <div class="thumbnail">
                 <img src="/images/patrocinadores/laraza.png" alt="NSC"/>
                 <p> ¡Gracias! </p>
                 </div>
                 </div>
                 
-               <div class="col-sm-3">
+                <div class="col-sm-2">
+                <div class="thumbnail">
+                <img src="/images/patrocinadores/puentes.png" alt="NSC"/>
+                <p> ¡Gracias! </p>
+                </div>
+                </div>
+                
+                <div class="col-sm-2">
+                <div class="thumbnail">
+                <img src="/images/patrocinadores/puentes.png" alt="NSC"/>
+                <p> ¡Gracias! </p>
+                </div>
+                </div>
+                
+                <div class="col-sm-2">
+                <div class="thumbnail">
+                <img src="/images/patrocinadores/latinoAdvocacy.jpg" alt="NSC"/>
+                <p> ¡Gracias! </p>
+                </div>
+                </div>
+                
+                <div class="col-sm-2">
                 <div class="thumbnail">
                 <img src="/images/patrocinadores/NSC.png" alt="NSC"/>
                 <p> ¡Gracias! </p>
                 </div>
                 </div>
                 
-                 <div class="col-sm-3">
+                <div class="col-sm-2">
+                <div class="thumbnail">
+                <img src="/images/patrocinadores/NSC.png" alt="NSC"/>
+                <p> ¡Gracias! </p>
+                </div>
+             </div>
+          </div>  <!---End ROw -->
+          
+          
+       <!---Patrocinadores 2nd--->    
+                <div class="row">
+                <div class="col-sm-2">
+                <div class="thumbnail">
+                <img src="/images/patrocinadores/laraza.png" alt="NSC"/>
+                <p> ¡Gracias! </p>
+                </div>
+                </div>
+                
+                <div class="col-sm-2">
+                <div class="thumbnail">
+                <img src="/images/patrocinadores/puentes.png" alt="NSC"/>
+                <p> ¡Gracias! </p>
+                </div>
+                </div>
+                
+                <div class="col-sm-2">
+                <div class="thumbnail">
+                <img src="/images/patrocinadores/puentes.png" alt="NSC"/>
+                <p> ¡Gracias! </p>
+                </div>
+                </div>
+                
+                <div class="col-sm-2">
                 <div class="thumbnail">
                 <img src="/images/patrocinadores/NSC.png" alt="NSC"/>
                 <p> ¡Gracias! </p>
                 </div>
                 </div>
                 
-                <div class="col-sm-3">
+                <div class="col-sm-2">
+                <div class="thumbnail">
+                <img src="/images/patrocinadores/NSC.png" alt="NSC"/>
+                <p> ¡Gracias! </p>
+                </div>
+                </div>
+                
+                <div class="col-sm-2">
                 <div class="thumbnail">
                 <img src="/images/patrocinadores/NSC.png" alt="NSC"/>
                 <p> ¡Gracias! </p>

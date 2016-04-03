02a7e45b9867489186d1d589fb4bed1bb2d03ebd
Perla Mendoza <pmdz70@gmail.com>
2016-04-02 18:03:34 -0700

    agregue maru foto

--- resources/views/pages/conferencistas.blade.php
+++ resources/views/pages/conferencistas.blade.php
@@ -15,7 +15,7 @@
             <div class="row">
                 <div class="col-xs-6 col-xs-offset-3 col-sm-3 col-sm-offset-1 col-md-4 col-md-offset-1  col-lg-4 col-lg-offset-1">
                     <div class="conferencistas1">
-                        <img  src="images/conferencistasMaru.jpeg"  class="img-responsive" alt="maria" width="250" height="200"/>
+                        <img  src="images/conferencistasMaru.jpg"  class="img-responsive" alt="maria" width="250" height="200"/>
                     </div>
                 </div>
                 <div class="col-xs-12 col-sm-7 col-md-6 col-lg-6">

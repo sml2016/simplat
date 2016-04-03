264b06312f6cab780c6a9fea99a6d8720380fab7
Luis Garcia Alanis <luis@garcia.tv>
2016-04-03 15:02:14 -0700

    Leer mas

--- public/js/all.js
+++ public/js/all.js
@@ -71,4 +71,35 @@ function RegistroInit() {
     });
 }
 
+$(document).ready(function() {
+    var showlength = 100;
+    
+    $('.readMore').each(function() {
+        var content = $(this).html();
+        var showlengthTmp = $(this).attr('showlength');
+        if (showlengthTmp) showlength = showlengthTmp;
+ 
+        if(content.length > showlength) {
+            var shortText = content.substr(0, showlength);
+            var extraText = content.substr(showlength, content.length - showlength);
+ 
+            var html = shortText + '<span class="extraText" style="display: none">' + extraText + '<span>';
+ 
+            $(this).html(html);
+        }
+ 
+    });
+
+    $('.readMoreLessButton').click(function () {
+        $(this).parent().parent().children('.readMore').children('.extraText').toggle();
+        $(this).parent().children('.readMoreButton').toggle();
+        $(this).parent().children('.readLessButton').toggle();
+        return false;
+    });
+
+    $('.readLessButton').click(function () {
+        $(this).parent().parent().children('.readMore').children('.extraText').toggle();
+        return false;
+    });
+});
 //# sourceMappingURL=all.js.map
--- resources/views/pages/conferencistas.blade.php
+++ resources/views/pages/conferencistas.blade.php
@@ -21,13 +21,16 @@
                     <div class="conferencistas1a">
                         <h2>Maribel Meza Tomara <span class="bio"> Biografía</span></h2>
                         <h3>Maestra en la Universidad de Washington</h3>
-                        <p>English writer Virginia Woolf was raised in a remarkable household. Her father, Sir Leslie Stephen, was an historian and author, and also one of the most prominent figures in the golden age of mountaineering. Woolf’s mother, Julia Prinsep Stephen (née Jackson), had been born in India and later served as a model for several Pre-Raphaelite painters. She was also a nurse and wrote a book on the profession. Woolf had three full siblings and four half-siblings; both of her parents had been married and widowed before marrying each other. The eight children lived under one roof at 22 Hyde Park Gate, Kensington.
-                            <div id="leermas" class="visible-xs visible-sm hidden-md hidden-lg">
-                                <button type="button" class="btn btn-leer btn-sm">Leer más</button>
-                            </div>
+                        <p class="readMore" showlength="611">English writer Virginia Woolf was raised in a remarkable household. Her father, Sir Leslie Stephen, was an historian and author, and also one of the most prominent figures in the golden age of mountaineering. Woolf’s mother, Julia Prinsep Stephen (née Jackson), had been born in India and later served as a model for several Pre-Raphaelite painters. She was also a nurse and wrote a book on the profession. Woolf had three full siblings and four half-siblings; both of her parents had been married and widowed before marrying each other. The eight children lived under one roof at 22 Hyde Park Gate, Kensington.
+                            <br>
                             Two of Woolf’s brothers had been educated at Cambridge, but all the girls were taught at home and utilized the splendid confines of the family’s lush Victorian library. Moreover, Woolf’s parents were extremely well connected, both socially and artistically. Her father was a friend to William Thackeray and George Henry Lewes, as well as many other noted thinkers. Her mother’s aunt was the famous 19th century photographer Julia Margaret Cameron. For these reasons and more, Virginia Woolf was ideally situated to appreciate and experiment with the art of writing.
-                        From the time of her birth, on January 25, 1882, until 1895, Woolf spent her summers in St. Ives, a beach town at the very southwestern tip of England. The Stephens’ summer home, Talland House, which is still standing today, looks out at the dramatic Porthminster Bay and has a view of the Godrevy lighthouse, which inspired her writing. In her later memoirs, Woolf recalled St. Ives with a great fondness. In fact, she incorporated scenes from those early summers into her modernist novel, To the Lighthouse (1927).
-                    </p>
+                            <br>
+                            From the time of her birth, on January 25, 1882, until 1895, Woolf spent her summers in St. Ives, a beach town at the very southwestern tip of England. The Stephens’ summer home, Talland House, which is still standing today, looks out at the dramatic Porthminster Bay and has a view of the Godrevy lighthouse, which inspired her writing. In her later memoirs, Woolf recalled St. Ives with a great fondness. In fact, she incorporated scenes from those early summers into her modernist novel, To the Lighthouse (1927).
+                        </p>
+                        <div id="leermas" class="visible-xs visible-sm hidden-md hidden-lg">
+                            <button type="button" class="btn btn-leer btn-sm readMoreLessButton">Leer más</button>
+                            <button type="button" class="btn btn-leer btn-sm readMoreLessButton" style="display:none">Leer menos</button>
+                        </div>
                     </div>
                 </div>
             </div> <!--End ROW -->
@@ -42,13 +45,14 @@
                     <div class="conferencistas1a">
                         <h2>Maribel Meza Tomara <span class="bio"> Biografía</span></h2>
                         <h3>Maestra en la Universidad de Washington</h3>
-                        <p>English writer Virginia Woolf was raised in a remarkable household. Her father, Sir Leslie Stephen, was an historian and author, and also one of the most prominent figures in the golden age of mountaineering. Woolf’s mother, Julia Prinsep Stephen (née Jackson), had been born in India and later served as a model for several Pre-Raphaelite painters. She was also a nurse and wrote a book on the profession. Woolf had three full siblings and four half-siblings; both of her parents had been married and widowed before marrying each other. The eight children lived under one roof at 22 Hyde Park Gate, Kensington.
-                            <div id="leermas" class="visible-xs visible-sm hidden-md hidden-lg">
-                                <button type="button" class="btn btn-leer btn-sm">Leer más</button>
-                            </div>
+                        <p class="readMore" showlength="611">English writer Virginia Woolf was raised in a remarkable household. Her father, Sir Leslie Stephen, was an historian and author, and also one of the most prominent figures in the golden age of mountaineering. Woolf’s mother, Julia Prinsep Stephen (née Jackson), had been born in India and later served as a model for several Pre-Raphaelite painters. She was also a nurse and wrote a book on the profession. Woolf had three full siblings and four half-siblings; both of her parents had been married and widowed before marrying each other. The eight children lived under one roof at 22 Hyde Park Gate, Kensington.
                             Two of Woolf’s brothers had been educated at Cambridge, but all the girls were taught at home and utilized the splendid confines of the family’s lush Victorian library. Moreover, Woolf’s parents were extremely well connected, both socially and artistically. Her father was a friend to William Thackeray and George Henry Lewes, as well as many other noted thinkers. Her mother’s aunt was the famous 19th century photographer Julia Margaret Cameron. For these reasons and more, Virginia Woolf was ideally situated to appreciate and experiment with the art of writing.
                         From the time of her birth, on January 25, 1882, until 1895, Woolf spent her summers in St. Ives, a beach town at the very southwestern tip of England. The Stephens’ summer home, Talland House, which is still standing today, looks out at the dramatic Porthminster Bay and has a view of the Godrevy lighthouse, which inspired her writing. In her later memoirs, Woolf recalled St. Ives with a great fondness. In fact, she incorporated scenes from those early summers into her modernist novel, To the Lighthouse (1927).
-                    </p>
+                        </p>
+                        <div id="leermas" class="visible-xs visible-sm hidden-md hidden-lg">
+                            <button type="button" class="btn btn-leer btn-sm readMoreLessButton">Leer más</button>
+                            <button type="button" class="btn btn-leer btn-sm readMoreLessButton" style="display:none">Leer menos</button>
+                        </div>
                     </div>
                 </div>
             </div> <!--End ROW -->
@@ -106,3 +110,4 @@
     </div>
 </div>
 @stop
+

e27cb93da2177f6fe9b4930135ff1bd0cae858ca
Perla Mendoza <pmdz70@gmail.com>
2016-04-03 16:17:21 -0700

    traduccion de la pag de mensaje enviado de contacto

---
+++ resources/lang/en/contactoEnviado.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+'mensajeEnviado' => '<h1>Your message has been successfully sent.</h1>
+            <h2>We will contact you as soon as possible.</h2>
+            <h3>Thank you for contacting us!</h3>'
+       
+];

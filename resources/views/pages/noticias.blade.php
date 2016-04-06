0d410ec5a5b2350f40c81b7906a4ca278007b3e4
Perla Mendoza <pmdz70@gmail.com>
2016-04-06 13:14:47 -0700

    gregando noticias

--- resources/views/pages/noticias.blade.php
+++ resources/views/pages/noticias.blade.php
@@ -11,11 +11,18 @@
         <h1> Maru Mora Villalpando:<br>
          Galardonada con el premio Puño de Oro</h1>
           <h2>El Tercer Simposio de Mujeres Latinas acordó por unanimidad entregar el premio “Puño de Oro” a la activista política Maru Mora Villalpando. Su trabajo como creadora de Latino Advocacy para defender los derechos humanos de la comunidad latina y las luchas raciales de esta minoría, la hacen merecedora de este reconocimiento entregado a mujeres que son un ejemplo en su comunidad.</h2>
-        <h3>Amarilis Aranguren Sánchez <span class="grey"> / </span> April 08, 2016</h3>
+        <h3>Amarilis Aranguren Sánchez <span class="grey"> / </span> April 11, 2016</h3>
       </div><!-- END News20161 -->
       
       <div class="row">
-      <div class="col-md-8 col-lg-8">
+       <div class="col-sm-5 col-md-4 col-lg-4">    
+        <figure class="figure">
+          <img src="/images/NewsMaru.jpeg"  <img class="img-responsive" alt="Maru Villalpando">
+         <figcaption class="figure-caption">Gráfico de Internet</figcaption>
+        </figure>
+       </div> <!-- END Col -->
+       
+      <div class="col-sm-7 col-md-8 col-lg-8">
       <article>
        <div class="collapsable" collapsed-height="280">    
         <p><span class="CapitalLetter">M</span>aru Mora Villalpando llegó a los Estados Unidos en 1992, desde México, asustada por la violencia desatada en su país por la inestabilidad política. Su experiencia como indocumentada tras más de 20 años en Seattle la llevó a dedicar su vida a ayudar a otros en su situación.</p>
@@ -31,15 +38,44 @@
         </div>
       </article>  
       </div>
-     
-      <div class="col-md-4 col-lg-4">    
+      </div><!-- END ROw --> 
+      
+      <div class="news20162">
+      <h1>“Reinventándonos después de migrar”.</br>
+      Es el tema del Tercer Simposio de Mujeres Latinas</h1>
+      <h2>Todas las familias que han llegado a los Estados Unidos han tenido que pasar por la situación de adaptar sus vidas a costumbres diferentes. Tomando en consideración que la temática de la migración está tan vinculada con la comunidad latina en Seattle, estado de Washington, el Tercer Simposio de Mujeres Latinas estará dedicado este año a abordar esta realidad.</h2>
+      <h3>Amarilis Aranguren Sánchez <span class="grey"> / </span> April 04, 2016</h3>
+      </div><!-- END News20162 --> 
+      
+      <div class="row">
+       <div class="col-sm-5 col-md-4 col-lg-4">    
         <figure class="figure">
-          <img src="/images/NewsMaru.jpeg" class="figure-img img-fluid" alt="Maru Villalpando">
+          <img src="/images/NewsMaru.jpeg"  <img class="img-responsive" alt="Maru Villalpando">
          <figcaption class="figure-caption">Gráfico de Internet</figcaption>
         </figure>
        </div> <!-- END Col -->
+     
+     <div class="col-sm-7 col-md-8 col-lg-8">
+      <article>
+       <div class="collapsable" collapsed-height="280">    
+        <p>“<span class="CapitalLetter">R</span>einventándonos después de migrar” es el tema alrededor del cual panelistas y conferencistas contarán a las 225 participantes del evento que sí es posible salir exitosas de esta transición. El proceso de adaptarse a una nueva cultura, a un nuevo ambiente, sin dejar de lado las costumbres que cada quien trae de su país y de sus familias, es el reto que la comunidad latina enfrenta día a día.</p>
+        <p>De acuerdo a un informe de la Organización de Naciones Unidas, en los Estados Unidos viven 26 millones de inmigrantes latinoamericanos. Se trata de un gran grupo de personas que lucha día a día contra el racismo y la discriminación, esforzándose para tener una vida mejor.
+        El proceso de instalarse en otro país sea por vías legales o no, genera en las familias una situación traumática que implica el distanciamiento físico de seres queridos y familiares. Las mujeres latinas tienen un rol importante en el proceso de adaptación de sus familias a un nuevo estilo de vida.</p>
+        <p>El 21 de mayo de 2016, la activista por los derechos humanos de los latinos Maru Mora Villalpando contará cómo ha sido su experiencia en la defensa de la comunidad, su rol como fundadora de Latino Advocacy y mostrará como el liderazgo de las mujeres puede transformar realidades. </p>
+       <p>Maru Mora Villalpando recibirá el reconocimiento “Puño de Oro” otorgado por el Simposio de Mujeres Latinas y estará a cargo de la conferencia principal del evento.</p>
+       <p></p>
+       <p>También participará como conferencista María-José “Cote” Soerens, directora ejecutiva de Puentes, una organización que moviliza recursos para ayudar psicológicamente a los latinos indocumentados en el estado deWashington. Así como Lizbeth Gutiérrez, candidata a Ph.D. en Washington State University y especialista en estudios sobre la mujer.</p>
+       <p>La trabajadora social Elodia González mostrará con su ejemplo como el trabajo duro puede cambiar la vida de todos los que migran. Gonzáles ayuda a la comunidad latina desde Seamar y otras prácticas a mejorar sus problemas de ansiedad. Su historia mostrará la capacidad de transformación que tienen las mujeres, pues después de tener a sus hijos reinventó su carrera profesional hasta convertirse en un aporte invaluable a la comunidad.</p>
+       <p>El Tercer Simposio de Mujeres Latinas quiere impulsar a todas las mujeres que migraron a este país e inspirarlas a lograr cada día más, pues la vida no termina al llegar a Estados Unidos. Ese es el principio de algo mejor. “Reinventándonos después de migrar” será una muestra de lo que es capaz la mujer latina con su tenacidad.</p>
+       <p>La invitación es a participar en el Tercer Simposio de Mujeres Latinas que se realizará en las instalaciones del North Seattle College, el próximo sábado 21 de mayo de 2016. 225 mujeres tendrán la oportunidad de participar en este evento gratuito que se llevará a cabo dentro las 10:00 am y 3:30 pm. </p>
+        </div>
+        <div id="leermas" class="" style="display:block;">
+           <button type="button" class="btn btn-leer btn-sm readMoreButton">Leer más</button>
+         <button type="button" class="btn btn-leer btn-sm readLessButton" style="display:none">Leer menos</button>
+        </div>
+       </article>  
+       </div>
       </div><!-- END ROw --> 
-       
        
       </div><!-- END Noticias -->  
      </div><!-- END fondo1 -->

2afe6f38fed82f1635b21e0300f7e30be510fed9
Perla Mendoza <pmdz70@gmail.com>
2016-04-09 03:13:22 -0700

    testimonios

--- resources/lang/en/testimonios.php
+++ resources/lang/en/testimonios.php
@@ -2,15 +2,21 @@
 
 return [
     'Titulo' => 'What women are saying',
-    'Dicen' => '<p>I take with me the feeling of being "home" for a few hours this morning with my "family" that sends me back to my normal life in Seattle but with many more tools and much affection.</p>',
-    'nombreXochi' => 'Xochipilli Alonso',
-    'Fecha' => 'May/18/2015',
-    'Dicen1' => '<p>“We, Latina women, we are the ones raising the future. The Latina symposium was a great opportunity to meet my sisters who daily strive to make a better future for our community and for our children. Together everything is possible".</p>',
+    'diceJueza' => '<p>“We, Latina women, we are the ones raising the future. The Latina symposium was a great opportunity to meet my sisters who daily strive to make a better future for our community and for our children. Together everything is possible".</p>',
     'nombreJueza' => 'Judge Veronica A. Galván',
-    'Dicen2' => '<p>"So many inspirational messages, it’s great a way to 
-                    develop the community sense between Latinas. It was really helpful the forum with the stories of the 4 women”</p>',
-    'nombreMaribel' => 'Maribel Pastor', 
-    'Dicen3' => '<p>“I learned to value my self even more, it was a day to reaffirm my plans and dreams, I came with no expectations, 
+     'Fecha' => 'May/16/2015',
+    'diceLupita' => '<p>Wonderful speakers, All with a special shade.</p>
+                     <p>From the First to the second Symposium, I have seen an 80% improvement. Thanks! I felt loved, supported and welcomed. Good team work.</p>',
+    'nombrelupita' => 'Lupita Zamora', 
+    'diceMaria' => '<p>Everything is different and similar at the same time because of the struggle (Internal conflict, language, cultural, food, even different education and religion).</p>
+                    <p>These success stories are a proof that when you really want something, you can achieve it.</p>',
+    'nombreMaria' => 'María Trinidad Hernández',
+   'diceAle' => '<p>The speech of Judge Veronica Galvan was excellent. Gladis Lasprilla was wonderful. Congratulations! The presentation of the artists and Leticia Nieto was great. </p> 
+                <p>Thanks to all you for organizing the Latinas Symposium.</p>',
+    'nombreAle' => 'Alejandra Hernández',
+    'diceXochi' => '<p>I take with me the feeling of being "home" for a few hours this morning with my "family" that sends me back to my normal life in Seattle but with many more tools and much affection.</p>',
+    'nombreXochi' => 'Xochipilli Alonso',
+    'diceMarcela' => '<p>“I learned to value my self even more, it was a day to reaffirm my plans and dreams, I came with no expectations, 
                     but the workshops were very inspiring".</p>',
     'nombreMarcela' => 'Marcela Diaz'                 
 ];

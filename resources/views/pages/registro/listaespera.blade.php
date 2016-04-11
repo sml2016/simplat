67b439959a09fa91e69c80fa1f26653f23b9e439
Perla Mendoza <pmdz70@gmail.com>
2016-04-11 16:06:22 -0700

    cambios en controller

--- resources/views/pages/registro/listaespera.blade.php
+++ resources/views/pages/registro/listaespera.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
   <div class="fondo1">
     <div class="lista">
-      <h1>Estas registrada en <b>"Nuestra Lista de Espera"</b> del 3er Simposio de Mujeres Latinas"</h1>
+      <h1>Estas registrada en "Nuestra Lista de Espera" del 3er Simposio de Mujeres Latinas"</h1>
       <h2>Que será el <span class="fecha1">sábado 21 de Mayo del 2016</span> a partir de las <span class="fecha1"> 10:30 a.m.</span> hasta<span class="fecha1">  las 3:00 p.m.</span>
       En las instalaciones de North Seattle College.</h2>
       <p>Nosotros nos comunicaremos contigo en caso de que tengamos un lugar para ti.</p>

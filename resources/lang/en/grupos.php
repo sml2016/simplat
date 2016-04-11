95e9bf8b174bed15903b69ed8ebc3aa50486f17d
Perla Mendoza <pmdz70@gmail.com>
2016-04-11 12:50:13 -0700

    cambios grupos

---
+++ resources/lang/en/grupos.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+  
+  'titulo' => 'Groups',
+  'yuri' => '<p>It was a beautiful experience being part of the “Simposio de Mujeres Latinas 2015”!
+Being surrounded by so many beautiful women in such a rich, positive and friendly environment, I felt I was in my own country with my family and friends (the ones I left when I came to the US).</p>
+         <p>Over nice conversation, laughter and sharing, I got so inspired that ideas to support our community started flowing in my mind immediately. As a result, that day, I started organizing a campaign to bring financial support, school supplies and toys to a small town in Mexico, and I succeeded. It is really true that nothing is impossible when we support each other!</p>
+         <p>One of the best benefits I got from the symposium was enrolling in the program - “Leadership and Personal Development”. During those nine months in the program, Laura Evelia and Edgar Cruz guided and supported our personal and professional development with love and patience. The results were amazing. I increased my potential, my skills and strengths as woman and as a leader. I got in love with the best version of myself. I took the challenge to find a daily balance in every area of my life that makes me and the ones around me happy.</p>',
+  'yuri1' => 'With love and respect,<br>
+             Yuri Jenson',
+  'yurifecha' => 'April/05l/2016',
+  'maria' => '<p>My name is Maria Concepción, I am mother of three children. Last year I was invited to the “Simposio de Mujeres Latinas”. I was very excited to attend it. The day of the event, from the moment I left home, I felt “free”. It had been long time since I did something I like to do, because I had kept myself busy with work and family. I felt very happy that I could spend time with my kids after work. And without noticing it, I forgot to keep time for me.</p>
+        <p>Thanks to the symposium, I found myself again. It was there where I got to know about theprogram - “Leadership and Personal Development”. This is a nine-month training. After the symposium, I attended the leadership program. I highly recommend it. It has helped me a lot. I got motivation and willingness to keep learning.</p> 
+        <p>I want to thank the coordinators of the symposium, for giving their valuable time to make events like the “Simpisio de Mujeres Latinas” possible. The speakers and guests make us feel emotionally related with them and their experiences. They motivate us to keep going forward as people, as Latinas and as women.</p>',
+ 'maria1' => 'María Concepción',
+ 'mariafecha' => 'April/01/2016' 
+  
+  
+];
---
+++ resources/lang/es/grupos.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+  
+  'titulo' => 'Grupos de Seguimiento',
+  'yuri' => '<p>¡Fue una hermosa experiencia el haber sido parte del Simposio de Mujeres Latinas 2015! Estar rodeada de tantas mujeres bellas y de un ambiente tan enriquecedor, divertido, positivo y amistoso, me hizo sentir como si estuviera en mi país con mi familia y amigos 
+            que deje al venir me a Estados Unidos.</p>
+         <p>Entre risas y platicas amenas me inspire tanto que inmediatamente surgieron ideas paraapoyar mas a nuestra comunidad que me llevo a organizar en ese dia una campana para llevar apoyo financiero, útiles escolares y juguetes en un pueblo de México y lo logre. ¡Es verdad que nada parece imposible cuando hay apoyo entre nosotras!</p>
+         <p>A parte de todos estos beneficios también) ese día me anime a inscribirme al programa de Liderazgo y Desarrollo personal de Regala que nos ofrecieron al final . Laura Evelia y Edgar Cruz con amor y paciencia nos guiaron y apoyaron por nueve meses en nuestro crecimiento personal y profesional. Los resultados fueron maravillosos porque incremente mi potencial, mis habilidades y fortalezas como mujer y como líder, me enamore de la mejor versión de misma y tome el reto de encontrar un balance en cada área de mi vida todos los días que me haga feliz y a los que me rodean. Definitivamente me he unido a su misión y visión; a empezar a trabajar en conjunto, a apoyándonos y respetándonos unos a los otros, a aprender diariamente algo nuevo a hacer un cambio positivo en nuestra comunidad y a lograr que cada Latin@ cumpla sus sueños en su vida.</p>',
+  'yuri1' => 'Con amor y respecto,<br>
+             Yuri Jenson',
+  'yurifecha' => '05/Abril/2016',
+  'maria' => '<p>Mi nombre es María Concepción y soy madre de tres niños, el año pasado me invitaron al Simposio de Mujeres Latinas y fui muy emocionada. Desde el momento en que salí de la casa me sentí "libre", ya tenía mucho tiempo que no hacia nada de lo que a mí me gustaba hacer, porque      solamente me dedique a trabajas y, después me enfoque a mi familia, que también me sentía feliz de dedicarles tiempos a ellos pero sin darme cuenta me olvide de mi.</p>
+        <p>Gracias al Simposio me volví a encontrar conmigo misma, ahí mismo me enteré del programa de "Liderazgo y Desarrollo Personal" que es un entrenamiento de nueve meses.</p> 
+        <p>Después de la conferencia asistí al entrenamiento, el cual recomiendo porque a mi me ha ayudado mucho,  y me ha servido de tener motivación y ganas de seguir aprendiendo.</p>
+        <p>Gracias a las coordinadoras por haberse tomado su valioso tiempo para hacer posible este tipo de eventos. Y gracias a sus invitadas nos sentimos conmovidas y animadas de seguir adelante sobre todo como latinas, como personas, como mujeres.</p>',
+ 'maria1' => 'María Concepción',
+ 'mariafecha' => '01/Abril/2016' 
+  
+  
+  
+];

ccf24a8adcf982ccb6368ceeb895cc9ea8b97816
Perla Mendoza <pmdz70@gmail.com>
2016-04-22 19:57:19 -0700

    foto sixta foote y noticia

--- resources/lang/es/footer.php
+++ resources/lang/es/footer.php
@@ -5,8 +5,8 @@ return [
     'Testimonios' => 'Testimonios',
     'Galeria2014' => 'Galería 2014',
     'Galeria2015' => 'Galería 2015',
-    'Conferencias2014' => 'Conferencias 2014',
-    'Conferencias2015' => 'Conferencias 2015',
+    'Conferencias2014' => 'Simposio 2014',
+    'Conferencias2015' => 'Simposio 2015',
     'Grupos' => 'Grupos',
     'Contacto' => 'Contacto',
     'test' => 'Provando localizacion'

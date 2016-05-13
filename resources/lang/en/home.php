5b7c8f4ed29942ae0bb72521f39e784b1a042547
Perla Mendoza <pmdz70@gmail.com>
2016-05-13 00:18:21 -0700

    lo de la orden del dia

--- resources/lang/en/home.php
+++ resources/lang/en/home.php
@@ -7,12 +7,47 @@ return [
      'BotonDonacion' => 'Donation',
     'BotonPatrocina' => 'Sponsor',
     'BotonInvolucrate' => 'Get Involved',
-    'VideoUrl' => 'https://player.vimeo.com/video/155624043',
-    'TeEsperamos' => '<h1>The 3rd Latin American Women Symposium is a free event!</br></h1>
+    'Agenda' => 'The agenda for the 3rd symposium',
+     'horario1' => '<p>10:30am - 10:55am</p>
+                 <p>10:30am - 11:00am</p>
+                 <p>11:00am – 11:05am<br></br></p>
+                 <p>11:05am - 11:10am<br></br></p>
+                 <p>11:10am – 12:00pm<br></br><br></br></p>
+                 <p>12:05pm – 12:35pm</p>
+                 <p>12:35pm - 12:50pm<br></br></p>
+                 <p>12:50pm - 01:20pm<br></br><br></br></p>
+                 <p>01:20pm - 01:50pm<br></br></p>
+                 <p><br>01:55pm - 02:30pm</br><br></br><br></br></p>
+                 <p>02:30pm - 02:50pm</p>
+                 <p>02:50pm - 03:05pm</p>
+                 <p>03:05pm - 03:10pm<br></br></p>
+                 <p>03:10pm - 04:00pm</p>',
+     'horario2' => '<p>Door will open and registration begin.</p>
+              <p>Time to visit information booth.</p>
+              <p>Welcome all! The master of ceremony will give us a summary of our day.</p>
+              <p>A few words from Dean Steve Thomas North Seattle College.</p>
+              <p>Premio Puño de Oro - Recognition for his work to empower the Latino community:
+                 <br><span>Maru Mora-Villalpando, founder of Latino Advocacy.</span></br></p>
+              <p>Lunch and Music by Enrique Carbajal García.</p>
+              <p>Seattle World Youth Orchestra - Paula Nava Madrigal Director of the Orchestra.</p>
+              <p>Panel: Chronicles  and stories of our personal travels:
+              <br><span>Elodia González, Mental Health Therapist.</span></br>
+                  <span>Ana Castro, Owner of Salvadorean Bakery.</span></b></p>
+              <p>Our health after immigrating:
+                  <br><span>María-José “Coté” Soerens. Founding executive director of Puentes.</span><br></p>
+              <p>Self-esteem after immigrating:
+                  <br><span>Lizeth Gutiérrez, Ph.D. candidate in the American Studies program in the Department of Critical Culture, Gender and Race Studies at Washington State University (WSU).</span></br></p> 
+              <p>Dialog at our tables.</p>
+              <p>Nicaraguan Ometepec Folklore Group.</p>
+              <p>Please complete the evaluation. Thank you for coming!</p>
+              <p>Time to visit the information booths.</p>',
+      'ordendeldiafoto' => '/images/ordendeldiaSMLingles.png',
+     'TeEsperamos' => '<h1>The 3rd Latin American Women Symposium is a free event!</br></h1>
                       <h2>To be held on <span class="fecha">Saturday May 21st </span>2016,
                       from <span class="fecha"> 10:30 AM to 3:00 PM</span>
                       at the North Seattle Community College.</h2>
                       <h3>Please Join Us!</h3>',
+    'VideoUrl' => 'https://player.vimeo.com/video/155624043',
     'ElPremio' => '<h1> Maru Mora Villalpando</h1>
                    <h2>For her contribution in the defense of Latin American immigrants\' rights and constant work empowering the Hispanic woman.
                    </br></br>We are honored to give this award to:
